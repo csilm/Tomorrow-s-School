86bc42632608285bdbb9ee14b148fd5586678fa7
Showrav-das <showravdas8@gmail.com>
2022-09-11 19:17:34 +0600

    testing

---
+++ FrontendFiles/sowrabdas/school/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import JoinUs from './components/JoinUs/JoinUs';
+
+function App() {
+  return (
+    <div className="App">
+      <JoinUs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontendFiles/sowrabdas/school/src/components/JoinUs/JoinUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JoinUs = () => {
+    return (
+        <div>
+            <h2>Join Us</h2>
+        </div>
+    );
+};
+
+export default JoinUs;
