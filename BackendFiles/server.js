58b02369d23605461a31a2560c0bf476175b1960
Tasfiq97 <tasfiqulazad200@gmail.com>
2022-09-12 16:46:52 +0600

    contact backend

--- BackendFiles/server.js
+++ BackendFiles/server.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const cors=require("cors")
+const port = 5000
+require('dotenv').config()
+
+app.use(cors());
+ app.use(express.json())
+ 
+//  need database related access 
+
+// for contact page
 
 
-app.get("/", function (req, res) { 
-    res.send('Hello Warriors');
- });
+app.get("/contact",(req,res)=>{
+    res.send("contact page")
+})
+app.post ("/contact",(req,res)=>{
+    res.send("contact add")
+})
 
-app.listen(3000, function () { 
-    console.log("server started on port 3000");
- });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
