c39fdcd05e5c270afb16d6e946267d7b72c6aedb
Yeakob <74756963+Yeakob@users.noreply.github.com>
2022-09-15 15:09:09 +0600

    Add files via upload
    What we do updated

---
+++ frontendfiles/src/What_WE_Do/All.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container2 from './Container2'
+import Container1 from './Container1'
+
+const All = () => {
+  return (
+    <div>
+      <Container1 />
+      <Container2 />
+    </div>
+  )
+}
+
+export default All
