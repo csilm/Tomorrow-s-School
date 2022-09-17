28b1cd03b9f2a38ded6f9e66225c018b987113a1
Yeakob <yeakob677@gmail.com>
2022-09-17 15:11:28 +0600

    Updated

--- frontendfiles/src/What_WE_Do/All.jsx
+++ frontendfiles/src/What_WE_Do/All.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Container2 from './Container2'
 import Container1 from './Container1'
@@ -13,19 +12,3 @@ const All = () => {
 }
 
 export default All
-=======
-import React from 'react'
-import Container2 from './Container2'
-import Container1 from './Container1'
-
-const All = () => {
-  return (
-    <div>
-      <Container1 />
-      <Container2 />
-    </div>
-  )
-}
-
-export default All
->>>>>>> c39fdcd05e5c270afb16d6e946267d7b72c6aedb
