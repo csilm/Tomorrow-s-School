9eb8f62fc097656a747d91af6d79fa78b568f5e7
Isfak Ahmed <82495635+asakib163@users.noreply.github.com>
2022-09-18 12:19:35 +0600

    Donate page commit

--- frontendfiles/src/App.js
+++ frontendfiles/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import JoinUs from './components/JoinUs/JoinUs';
-import Navigation from './shared/Navigation';
+// import JoinUs from './components/JoinUs/JoinUs';
+// import Navigation from './shared/Navigation';
+import Donate from './components/Donate/donate';
 
 function App() {
   return (
     <div className="App">
-    <Navigation/>
-     <JoinUs/>
+      <Donate/>
     </div>
   );
 }
