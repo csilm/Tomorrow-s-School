b44d3c383ab1920ba2609dce5435aab1fd349624
unknown <isfakahmed163@gmial.com>
2022-12-26 10:26:56 +0600

    mongod

--- BackendFiles/server.js
+++ BackendFiles/server.js
@@ -3,11 +3,11 @@ const cors=require("cors")
 const port = 5000 || process.env.PORT 
 const app =require("./app")
 const mongoose = require('mongoose')
-
+const mongodburl = "mongodb+srv://admin-isfak:<Test123>@cluster0.foygf8d.mongodb.net/?retryWrites=true&w=majority"
 require('dotenv').config()
 
 
-mongoose.connect('mongodb+srv://admin-isfak:<password>@cluster0.foygf8d.mongodb.net/?retryWrites=true&w=majority/tommorrow-school',{ useNewURLParser : true }).then(()=>{
+mongoose.connect(mongodburl,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
   console.log("database connected")
 })
 
