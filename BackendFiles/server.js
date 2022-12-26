8193c769dbaf51fd932bef6d9f981f5628ff3048
unknown <isfakahmed163@gmial.com>
2022-12-26 10:46:28 +0600

    change port

--- BackendFiles/server.js
+++ BackendFiles/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors=require("cors")
-const port = 5000 || process.env.PORT 
+const port = process.env.PORT 
 const app =require("./app")
 const mongoose = require('mongoose')
 const mongodburl = "mongodb+srv://admin-isfak:<Test123>@cluster0.foygf8d.mongodb.net/?retryWrites=true&w=majority"
 require('dotenv').config()
 
+mongoose.set('strictQuery', true);
 
 mongoose.connect(mongodburl,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
   console.log("database connected")
@@ -15,5 +16,5 @@ mongoose.connect(mongodburl,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at port ${port}.`)
 })
